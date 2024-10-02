ac9c818a30841cbb78196d2a994c11c549e2f584
nxquan <quananhb2410@gmail.com>
2024-10-02 11:55:16 +0700

    fix: remove .env file and create encryption key for each user

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -4,3 +4,9 @@ export const REPO_NAME_KEY = 'REPO_NAME';
 export const REPO_LINK_KEY = 'REPO_LINK';
 export const REPO_ID_KEY = 'REPO_ID';
 export const WORKING_DIR_KEY = 'WORKING_DIR';
+
+export const WEBSITE_URL = 'https://web-ssm.skydev.vn';
+export const API_URL = 'https://api-ssm.skydev.vn';
+export const APP_NAME = 'ssm-cli';
+export const ENV_VAULT = '.env.vault';
+export const PAT_FILENAME = 'pat.enc';
